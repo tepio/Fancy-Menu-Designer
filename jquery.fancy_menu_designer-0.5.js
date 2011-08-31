0cf943585a1e2693f3b7b4904c2fbf1b2f481c46
Ryan Schumacher <j.r.schumacher@gmail.com>
2011-08-31 11:38:05 -0500

    Added subproduct groups and some minor visual tweaks.

--- jquery.fancy_menu_designer-0.5.js
+++ jquery.fancy_menu_designer-0.5.js
@@ -7,7 +7,6 @@
   
   $.fancymenudesigner = function(structure, options, selector) {
     this.selector = selector;
-    console.log(structure);
     this.structure = typeof structure != 'object' ? {} : structure;
     this.options = $.extend({}, this.defaults, options);
     
@@ -81,30 +80,77 @@
             }
           });
         });
-    
-        /** Edit Title UI **/
+        
+        /** Subproduct Group Catalog UI **/
+        menu.selector.find('.subproduct_group-catalog li').livequery(function(){
+          $(this).draggable({
+            appendTo: 'body',
+            helper: 'clone',
+            cursorAt: {
+              top: 5,
+              left: 5
+            }
+          });
+        });
+        
+        /** Action UI **/
         menu.selector.find('header, header').live('mouseenter', function(e){
-          if(!$(this).children('div[class=~"-title"]').hasClass('edit-mode')) {
+          // Delete button
+          $(this).children('.delete').css('opacity', 0.5).show();
+          
+          // Options button
+          $(this).children('.options').css('opacity', 0.5).show();
+          
+          // Edit button
+          if(!$(this).children('div[class*="-title"]').hasClass('edit-mode')) {
             $(this).children('.edit-title').css('opacity', 0.5).show(); 
           }
         }).live('mouseleave', function(e){
-          if(!$(this).children('div[class=~"-title"]').hasClass('edit-mode')) {
+          // Delete button
+          $(this).children('.delete').hide();
+          
+          // Options button
+          $(this).children('.options').hide();
+          
+          if(!$(this).children('div[class*="-title"]').hasClass('edit-mode')) {
             $(this).children('.edit-title').hide(); 
           }
+        });
+        menu.selector.find('.delete,.options').live('mouseenter', function(e){
+          $(this).css('opacity', 1);
+        }).live('mouseleave', function(e){
+          $(this).css('opacity', 0.5);
         });
         menu.selector.find('.edit-title').live('mouseenter', function(e){
           $(this).css('opacity', 1);
         }).live('mouseleave', function(e){
-          if(!$(this).siblings('div[class=~"-title"]').hasClass('edit-mode')) {
+          if(!$(this).siblings('div[class*="-title"]').hasClass('edit-mode')) {
             $(this).css('opacity', 0.5);
           }
         }).live('click', function(e){
           e.preventDefault;
-          var $title = $(this).siblings('div[class=~"-title"]');
+          var $title = $(this).siblings('div[class*="-title"]');
           menu.eventFns.editTitle($title, e);
         });
         menu.selector.find('.title-change').live('keyup', function(e){
           menu.eventFns.editTitle($(this).parent(), e);
+        });
+        /* Delete */
+        menu.selector.find('.delete').live('click', function(e){
+          e.preventDefault();
+          var remove = confirm('Delete this section?');
+          if(remove) {
+            $(this).parent().parent().slideUp(function(){
+              $(this).remove();
+            });
+          }
+        });
+        /* Options */
+        menu.selector.find('.options').live('click', function(e){
+          e.preventDefault();
+          $(this).parent().siblings('.subproduct_group-rules').slideToggle(function() {
+            
+          });
         });
         
         /** Categories UI **/
@@ -138,7 +184,7 @@
               $(this).find(".placeholder").remove();
               
               menu.buildNode('product', ui.draggable.metadata({type:'attr',name:'data',single:'data'}))
-                .hide().appendTo(this).fadeIn();
+                .hide().appendTo(this).slideDown();
             }
           });
         });
@@ -158,13 +204,39 @@
             hoverClass: 'ui-state-hover',
             accept: '.catalog-item',
             greedy: true,
-            tolerance: 'touch',
+            tolerance: 'pointer',
             drop: function(event, ui) {
               // Remove jQuery UI placeholder
               $(this).find(".placeholder").remove();
               
               menu.buildNode('subproduct', ui.draggable.metadata({type:'attr',name:'data',single:'data'}))
-                .hide().appendTo(this).fadeIn();
+                .hide().appendTo(this).slideDown();
+            }
+          });
+        });
+        
+        /** Subproduct group UI **/
+        menu.selector.find('.subproduct_groups').livequery(function(){
+          $(this).sortable({
+            items: '.subproduct_group',
+            handle: '.grippy',
+            connectWith: '.subproduct_groups',
+            placeholder: 'list-placeholder',
+            sort: function(event, ui){
+              $( this ).removeClass( "ui-state-default" );
+            }
+          }).droppable({
+            activeClass: 'ui-state-default',
+            hoverClass: 'ui-state-hover',
+            accept: '.catalog-subproduct_group',
+            greedy: true,
+            tolerance: 'pointer',
+            drop: function(event, ui) {
+              // Remove jQuery UI placeholder
+              $(this).find(".placeholder").remove();
+              
+              menu.buildNode('subproduct_group', ui.draggable.metadata({type:'attr',name:'data',single:'data'}))
+                .hide().appendTo(this).slideDown();
             }
           });
         });
@@ -173,7 +245,7 @@
         menu.selector.find('.category-add_button').click(function(e){
           e.preventDefault();
           menu.buildNode('category', {})
-            .hide().insertAfter(this).fadeIn();
+            .hide().insertAfter(this).slideDown();
         });
     
       },
@@ -195,7 +267,8 @@
         
         // Categories
         var category_order = 0;
-        menu.selector.find('.category').each(function(){
+        menu_container = menu.selector.find('.menu:not(.template)');
+        menu_container.find('.category').each(function(){
           var meta = $(this).metadata();
           var title = $(this).find('.category-title').text();
           
@@ -230,15 +303,21 @@
               var title = $(this).find('.subproduct_group-title').text();
               // @TODO Build out the rules
               //var rules = $(this)....
+              var rules = {};
+              $(this).find('.subproduct_group-rules .rule').each(function(){
+                var rule = $(this).find('input');
+                rules[rule.attr('name')] = rule.val();
+              });
+              
               structure.categories[category_order - 1].products[product_order - 1].subproduct_groups.push({
                 id: typeof meta.id == undefined ? null : meta.id,
                 title: title,
-                //rules: rules,
+                rules: rules,
                 subproducts: [],
                 order: subproduct_group_order++
               });
               
-              structure.categories[category_order - 1].products[product_order - 1].subproduct_groups[subproduct_group_order - 1] = menu.buildSubproducts($(this).find('.subproduct'));
+              structure.categories[category_order - 1].products[product_order - 1].subproduct_groups[subproduct_group_order - 1].subproducts = menu.buildSubproducts($(this).find('.subproduct'));
             });
             
           });
@@ -291,16 +370,20 @@
         var options = $.extend({}, {
           callback: null,
           callback_results: 'children'
-        }, opts);
-        
+        }, opts);        
               
-        $objs.each(function(){
+        $objs.each(function(){          
           var meta = $(this).metadata({type:'attr', name:'data',single:'data'});
           var type = 'toggle';
-          var value = 'on';
+          var value = 'off';
           var rules = {
             required: false
           };
+          
+          if($(this).find('header .default input:checkbox').val()) {
+            type = 'toggle';
+            value = $(this).find('header .default input:checkbox:checked').val() ? 'on' : 'off';
+          }
           
           var i = result.push({
             id: meta.id,
